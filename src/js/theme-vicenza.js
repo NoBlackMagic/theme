f59b4c8354be90260ea9820870facd6bdaee0085
thepeg <marco.pegoraro@gmail.com>
2014-02-04 20:10:19 +0100

    responsive behavior ok webkit

--- src/js/theme-vicenza.js
+++ src/js/theme-vicenza.js
@@ -1,6 +1,8 @@
 (function($){
 	
-	$('#side').click(function() {
+	$('#side').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
         $('body').toggleClass('side-open');
     });
 	
