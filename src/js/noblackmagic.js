16c847b16a0c6e73b40672636f9c7e85e6dbc466
thepeg <marco.pegoraro@gmail.com>
2014-02-07 08:16:25 +0100

    update javascript with DOM helpers

--- src/js/noblackmagic.js
+++ src/js/noblackmagic.js
@@ -1,12 +1,44 @@
 (function(){
 	
-	document.querySelector('#handler').addEventListener('click', function(e) {
-		var body = document.querySelector('body');
-		if (body.className.indexOf('side-open') === -1) {
-			body.className += ' side-open';
-		} else {
-			body.className = body.className.replace(' side-open', '');
-		}
+	var body = document.querySelector('body');
+	var handler = document.querySelector('#handler');
+	
+	
+	handler.addEventListener('click', function(e) {
+		toggleBody();
 	});
+	
+	
+	
+	function toggleBody() {
+		body.toggleClass('side-open');
+	}
+	
+	
+	
+	Element.prototype.hasClass = function(name) {
+		return new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)").test(this.className);
+	};
+	
+	Element.prototype.addClass = function(name) {
+		if (!this.hasClass(name)) {
+		    this.className = this.className ? [this.className, name].join(' ') : name;
+		}
+	};
+	
+	Element.prototype.removeClass = function(name) {
+		if (this.hasClass(name)) {
+		    var c = this.className;
+		    this.className = c.replace(new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)", "g"), "");
+		}
+	};
+	
+	Element.prototype.toggleClass = function(name) {
+		if (this.hasClass(name)) {
+			this.removeClass(name);
+		} else {
+			this.addClass(name);
+		}
+	};
 	
 })();
